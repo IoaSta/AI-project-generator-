4fb6c233fa11b17ab03ed7a18ffcb8a645d8b9aa
IoaSta <istavrati@gmail.com>
2025-09-23 21:55:20 +0200

    Added basic JS (form and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "We are all debts owed to death",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
