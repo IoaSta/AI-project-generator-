8410ff0a5a9b927d34fc775c9f893206b1749d04
IoaSta <istavrati@gmail.com>
2025-09-23 23:10:15 +0200

    Added AI structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "We are all debts owed to death",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "b319bb0553to0644aae8dd09f2b4aeba";
+  let context =
+    "You are a professor and love to read and quote famous people. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Provide a short quote about ${instructionsInput.value}`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
