ff4faeeeb36657c3f80a51815b211cb3fbb5f043
IoaSta <istavrati@gmail.com>
2025-09-24 14:59:23 +0200

    improved user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generateQuote(event) {
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden)");
+  quoteElement.innerHTML = `<div class="generating">⏳ thinking of a quote about ${instructionsInput.value}</div>`;
+
   axios.get(apiURL).then(displayQuote);
 }
 
